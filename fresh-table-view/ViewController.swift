43525bd14eff0ac92d42af61e3a1142aa74343b4
TGrooves-208 <tgrooves.208@gmail.com>
2016-02-23 21:44:27 -0700

    custom view set, custom cells, rounded corners on images, and minimum size of font set to 10px.

--- fresh-table-view/ViewController.swift
+++ fresh-table-view/ViewController.swift
@@ -8,18 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var uglyThings = ["https://s-media-cache-ak0.pinimg.com/originals/f1/9a/51/f19a5199180cc1f5c82bb5367fca65b8.jpg", "http://i.telegraph.co.uk/multimedia/archive/01565/blobfish_1565953c.jpg","http://application.denofgeek.com/images/gb/25bb/gremlins2.jpg","https://i.ytimg.com/vi/tSEl8Xlmv70/hqdefault.jpg", "http://2.bp.blogspot.com/_ym-eqNRfJpA/SrQ9KqvREEI/AAAAAAAAEaA/UzVYGyyan9M/s400/Ugly_dog.jpg"]
+    
+    var uglyTitles = ["Man this is uuuugly!", "Oh hell no!", "WTF is this!","This is nasty!", "Ewwwww!"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        if let cell = tableView.dequeueReusableCellWithIdentifier("UglyCell")
+            as? UglyCell {
+                
+                var img: UIImage!
+                
+                let url = NSURL(string: uglyThings[indexPath.row])!
+                
+                if let data = NSData(contentsOfURL: url) {
+                    img = UIImage(data: data)
+                    
+                } else {
+                    img = UIImage(named: "eels")
+                }
+                
+                cell.configureCell(img, text: uglyTitles[indexPath.row])
+                
+                return cell
+        } else {
+            return UglyCell()
+        }
     }
-
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return uglyThings.count
+    }
 
 }
+
+
 
